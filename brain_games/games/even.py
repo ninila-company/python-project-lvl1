0b5a4858d65d71533a020ba4f7472a9a9ea2ffa1
Ninila <ninila_company@bk.ru>
2021-01-25 17:12:24 +0500

    add def parity_check

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,4 +1,5 @@
 import prompt
+import random
 
 
 def check_even():
@@ -6,31 +7,23 @@ def check_even():
     name = prompt.string('May I have your name? ')
     print('Hello, {}'.format(name))
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    print('Question: 15')
-    while True:
+
+    count = 0
+    while count < 3:
+        number_random = random.randint(1, 100)
+        print(f'Question: {number_random}')
+
+        def parity_check(number):
+            return 'yes' if number % 2 == 0 else 'no'
+
         answer_one = prompt.string('Your answer: ').lower()
-        if answer_one != 'no':
+        if answer_one != parity_check(number_random):
             print(f"{answer_one} is wrong answer ;(. Correct answer was 'no'.")
             print(f"Let's try again, {name}!")
             break
         else:
             print('Correct')
-        print('Question: 6')
-        answer_two = prompt.string('Your answer: ').lower()
-        if answer_two != 'yes':
-            print(f"{answer_two} is wrong answer ;(. Correct answer was 'no'.")
-            print(f"Let's try again, {name}!")
-            break
-        else:
-            print('Correct')
-        print('Question: 7')
-        answer_three = prompt.string('Your answer: ').lower()
-        if answer_three != 'no':
-            print(f"{answer_three} is wrong answer ;(. "
-                  f"Correct answer was 'no'.")
-            print(f"Let's try again, {name}!")
-            break
-        else:
-            print('Correct')
+            count += 1
+
+    else:
         print(f'Congratulations, {name}')
-        break
